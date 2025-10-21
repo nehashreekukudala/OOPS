432b4aef9f65a547ad451f31ebdf12bc64af6c0c
nehashreekukudala <kn24csb0a36@student.nitw.ac.in>
2025-10-21 10:23:42 +0530

    Implement main class for Event Management System

---
+++ EventManagementSystem.java
@@ -0,0 +1,19 @@
+// EventManagementSystem.java
+package main;
+
+import view.LoginFrame;
+import javax.swing.*;
+
+public class EventManagementSystem {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeel());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
